29a6282c85fb1b3a8cdd1d243ae73485c056553a
yunhwan-dev <wjsdbsghks96@dgu.ac.kr>
2024-04-18 02:54:52 +0900

    feat : custom logging filter

--- security-example/src/main/kotlin/com/example/securityexample/SecurityExampleApplication.kt
+++ security-example/src/main/kotlin/com/example/securityexample/SecurityExampleApplication.kt
@@ -1,10 +1,15 @@
 package com.example.securityexample
 
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class SecurityExampleApplication
+
+
+inline fun <reified T> T.logger() = LoggerFactory.getLogger(T::class.java)!!
+
 
 fun main(args: Array<String>) {
     runApplication<SecurityExampleApplication>(*args)
--- security-example/src/main/kotlin/com/example/securityexample/config/SecurityConfig.kt
+++ security-example/src/main/kotlin/com/example/securityexample/config/SecurityConfig.kt
@@ -1,9 +1,17 @@
 package com.example.securityexample.config
 
+import com.example.securityexample.logger
+import jakarta.servlet.Filter
+import jakarta.servlet.FilterChain
+import jakarta.servlet.ServletRequest
+import jakarta.servlet.ServletResponse
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.web.SecurityFilterChain
 
 
@@ -31,5 +39,36 @@ class SecurityConfig {
         }
 
         return http.build();
+    }
+}
+
+class LoggingFilter : Filter {
+
+    override fun doFilter(request: ServletRequest?, response: ServletResponse?, chain: FilterChain?) {
+
+        val req = request as HttpServletRequest
+        val res = response as HttpServletResponse
+
+        // 현재 사용자 정보 가져오기
+        val authentication = SecurityContextHolder.getContext().authentication
+
+        // 사용자 정보가 있다면 로그에 포함시키기
+        val userInfo = if (authentication != null && authentication.isAuthenticated) {
+            "User: ${authentication.name}, Roles: ${authentication.authorities.joinToString(", ")}"
+        } else {
+            "Anonymous User"
+        }
+
+
+        val path = req.requestURI
+        val method = req.method
+
+        val startTime = System.currentTimeMillis()
+        chain?.doFilter(request, response)
+        val endTime = System.currentTimeMillis()
+
+        val log = "[${method}] ${path} (${endTime - startTime}ms), $userInfo"
+
+        logger().info(log)
     }
 }
